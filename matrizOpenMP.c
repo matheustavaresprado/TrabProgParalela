bffb5e0a5e784160a7add552c3da5d4e6347c326
holandafelipeuff <felipeholanda@id.uff.br>
2019-09-23 19:07:14 -0300

    Coloquei o código OpenMP com 100x100

--- matrizOpenMP.c
+++ matrizOpenMP.c
@@ -2,37 +2,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printMatrix(double mat[2][2], int n, int m){
+#define SIZE 100
+
+void printMatrix(double mat[SIZE][SIZE]){
     int i, j;
-    for(i = 0; i < n; i++){
-        for(j = 0; j < m; j++){
+
+    printf("\n\nPrintando a matriz :D \n\n");
+
+    for(i = 0; i < SIZE; i++){
+        for(j = 0; j < SIZE; j++){
             printf(" %.2f ", mat[i][j]);
         }
         printf("\n");
     }
 }
 
+void inicializaMatrix(double mat[SIZE][SIZE]){
+
+    int i, j;
+    for(i = 0; i < SIZE; i++){
+        for(j = 0; j < SIZE; j++){
+            mat[i][j] = j + 1;
+        }
+    }
+}
+
 void main(){
 
-    int n = 2;
-    int m = 3;
+    double matriz1[SIZE][SIZE];
+    inicializaMatrix(matriz1);
 
-    double matriz1[n][m];
-    matriz1[0][0] = 2.0; matriz1[0][1] = 3.0; matriz1[0][2] = 1.0; matriz1[1][0] = -1.0; matriz1[1][1] = 0.0; matriz1[1][2] = 2.0; 
+    double matriz2[SIZE][SIZE];
+    inicializaMatrix(matriz2);
 
-    double matriz2[m][n];
-    matriz2[0][0] = 1.0; matriz2[0][1] = -2.0; matriz2[1][0] = 1.0; matriz2[1][1] = 2.0; matriz2[2][0] = 3.0; matriz2[2][1] = 1.0;
-    
-    double matriz3[n][n];
+    double matriz3[SIZE][SIZE];
 
     #pragma omp parallel 
     {
         int ID =  omp_get_thread_num();
         int i, j, k; 
         double sum = 0.0;
-        for(i = 0; i < n; i++){
-            for(j = 0; j < n; j++){
-                for(k = 0; k < m; k++){
+        for(i = 0; i < SIZE; i++){
+            for(j = 0; j < SIZE; j++){
+                for(k = 0; k < SIZE; k++){
                     sum += matriz1[i][k]*matriz2[k][j];
                     printf("%.2f - %.2f \n", matriz1[i][k], matriz2[k][j]);
                 }
@@ -44,13 +56,5 @@ void main(){
         printf("Eu, thread %d finalizei minhas contas\n", ID);
     }
 
-    printMatrix(matriz3, n, n);
-    /*
-    #pragma omp parallel
-    {
-        int ID =  omp_get_thread_num();
-        printf("hello %d", ID);
-        printf("world %d\n",ID);
-    }
-    */
+    printMatrix(matriz3);
 }
